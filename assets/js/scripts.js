3ff63cfb54983e8a8ad7c3da2af8e4b91c0caa03
stev232 <stev232@yahoo.com>
2022-12-25 11:48:38 -0600

    added functionality through javascript so that the user can click to clear the tile or hover to change the color

---
+++ assets/js/scripts.js
@@ -0,0 +1,403 @@
+const rowOne = document.getElementById("rowOne");
+const rowTwo = document.getElementById("rowTwo");
+const rowThree = document.getElementById("rowThree");
+const rowFour = document.getElementById("rowFour");
+
+const squares = ["#sqrOne", "#sqrTwo", "#sqrThree", "#sqrFour", "#sqrFive", "#sqrSix", "#sqrSeven", "#sqrEight", "#sqrNine", "#sqrTen", "#sqrEleven", "#sqrTwelve" ];
+
+var count = 0;
+
+var color = ["#FF0000", "#FF3300", "#FF7700", "#FFBB00", "#FFFF00", "#BBFF00", "#77FF00", "#33FF00", "#00FF00", "#00FF33", "#00FF77"
+, "#00FFBB", "#00FFFF", "#00BBFF", "#0077FF", "#0033FF", "#0000FF", "#3300FF", "#7700FF", "#BB00FF", "#FF00FF", "#FF00BB", "#FF0077", "#FF0033"];
+
+rowOne.querySelector(squares[0]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[0]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[0]).addEventListener("click", function() {
+    rowOne.querySelector(squares[0]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[1]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[1]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[1]).addEventListener("click", function() {
+    rowOne.querySelector(squares[1]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[2]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[2]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[2]).addEventListener("click", function() {
+    rowOne.querySelector(squares[2]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[3]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[3]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[3]).addEventListener("click", function() {
+    rowOne.querySelector(squares[3]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[4]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[4]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[4]).addEventListener("click", function() {
+    rowOne.querySelector(squares[4]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[5]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[5]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[5]).addEventListener("click", function() {
+    rowOne.querySelector(squares[5]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[6]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[6]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[6]).addEventListener("click", function() {
+    rowOne.querySelector(squares[6]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[7]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[7]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[7]).addEventListener("click", function() {
+    rowOne.querySelector(squares[7]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[8]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[8]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[8]).addEventListener("click", function() {
+    rowOne.querySelector(squares[8]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[9]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[9]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[9]).addEventListener("click", function() {
+    rowOne.querySelector(squares[9]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[10]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[10]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[10]).addEventListener("click", function() {
+    rowOne.querySelector(squares[10]).setAttribute("style", "background-color: ");
+});
+
+rowOne.querySelector(squares[11]).addEventListener("mouseover", function() {
+    rowOne.querySelector(squares[11]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowOne.querySelector(squares[11]).addEventListener("click", function() {
+    rowOne.querySelector(squares[11]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[0]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[0]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[0]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[0]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[1]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[1]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[1]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[1]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[2]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[2]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[2]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[2]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[3]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[3]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[3]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[3]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[4]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[4]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[4]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[4]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[5]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[5]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[5]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[5]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[6]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[6]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[6]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[6]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[7]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[7]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[7]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[7]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[8]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[8]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[8]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[8]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[9]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[9]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[9]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[9]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[10]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[10]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[10]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[10]).setAttribute("style", "background-color: ");
+});
+
+rowTwo.querySelector(squares[11]).addEventListener("mouseover", function() {
+    rowTwo.querySelector(squares[11]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowTwo.querySelector(squares[11]).addEventListener("click", function() {
+    rowTwo.querySelector(squares[11]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[0]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[0]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[0]).addEventListener("click", function() {
+    rowThree.querySelector(squares[0]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[1]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[1]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[1]).addEventListener("click", function() {
+    rowThree.querySelector(squares[1]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[2]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[2]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[2]).addEventListener("click", function() {
+    rowThree.querySelector(squares[2]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[3]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[3]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[3]).addEventListener("click", function() {
+    rowThree.querySelector(squares[3]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[4]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[4]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[4]).addEventListener("click", function() {
+    rowThree.querySelector(squares[4]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[5]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[5]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[5]).addEventListener("click", function() {
+    rowThree.querySelector(squares[5]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[6]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[6]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[6]).addEventListener("click", function() {
+    rowThree.querySelector(squares[6]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[7]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[7]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[7]).addEventListener("click", function() {
+    rowThree.querySelector(squares[7]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[8]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[8]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[8]).addEventListener("click", function() {
+    rowThree.querySelector(squares[8]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[9]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[9]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[9]).addEventListener("click", function() {
+    rowThree.querySelector(squares[9]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[10]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[10]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[10]).addEventListener("click", function() {
+    rowThree.querySelector(squares[10]).setAttribute("style", "background-color: ");
+});
+
+rowThree.querySelector(squares[11]).addEventListener("mouseover", function() {
+    rowThree.querySelector(squares[11]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowThree.querySelector(squares[11]).addEventListener("click", function() {
+    rowThree.querySelector(squares[11]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[0]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[0]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[0]).addEventListener("click", function() {
+    rowFour.querySelector(squares[0]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[1]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[1]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[1]).addEventListener("click", function() {
+    rowFour.querySelector(squares[1]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[2]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[2]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[2]).addEventListener("click", function() {
+    rowFour.querySelector(squares[2]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[3]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[3]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[3]).addEventListener("click", function() {
+    rowFour.querySelector(squares[3]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[4]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[4]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[4]).addEventListener("click", function() {
+    rowFour.querySelector(squares[4]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[5]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[5]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[5]).addEventListener("click", function() {
+    rowFour.querySelector(squares[5]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[6]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[6]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[6]).addEventListener("click", function() {
+    rowFour.querySelector(squares[6]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[7]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[7]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[7]).addEventListener("click", function() {
+    rowFour.querySelector(squares[7]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[8]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[8]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[8]).addEventListener("click", function() {
+    rowFour.querySelector(squares[8]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[9]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[9]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[9]).addEventListener("click", function() {
+    rowFour.querySelector(squares[9]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[10]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[10]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[10]).addEventListener("click", function() {
+    rowFour.querySelector(squares[10]).setAttribute("style", "background-color: ");
+});
+
+rowFour.querySelector(squares[11]).addEventListener("mouseover", function() {
+    rowFour.querySelector(squares[11]).setAttribute("style", "background-color: " + color[count]);
+    countChecker();
+});
+rowFour.querySelector(squares[11]).addEventListener("click", function() {
+    rowFour.querySelector(squares[11]).setAttribute("style", "background-color: ");
+});
+
+
+function countChecker() {
+    count++;
+    if(count >= color.length) {
+        count = 0;
+    }
+}
