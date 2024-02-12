5b12b6efa49ccd8c11a9a1ba8c540e26e3f62866
Stephen Merki <stephen.merki232@gmail.com>
2024-02-12 15:35:58 -0600

    Updated the JQuery so that instead of immediately refilling the square it will now leave it blank.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,22 @@
 var count = 0;
 var color = ["#FF0000", "#FF3300", "#FF7700", "#FFBB00", "#FFFF00", "#BBFF00", "#77FF00", "#33FF00", "#00FF00", "#00FF33", "#00FF77", 
     "#00FFBB", "#00FFFF", "#00BBFF", "#0077FF", "#0033FF", "#0000FF", "#3300FF", "#7700FF", "#BB00FF", "#FF00FF", "#FF00BB", "#FF0077", "#FF0033"];
-
-$('.square').on('click', function(event) {
-    event.preventDefault();
-    event.target.setAttribute("style", "background-color: ");
-});
+let currentRectangle = 'null';
 
 $('.square').hover(function(event) {
     event.preventDefault();
-    event.target.setAttribute("style", "background-color: " + color[count]);
-    countChecker();
+    if(currentRectangle == event.target) {
+        currentRectangle = 'null';
+    } else {
+        countChecker();
+        event.target.setAttribute("style", "background-color: " + color[count]);
+    }
+});
+
+$('.square').on('click', function(event) {
+    event.preventDefault();
+    currentRectangle = event.target;
+    event.target.setAttribute("style", "background-color: ");
 });
 
 function countChecker() {
